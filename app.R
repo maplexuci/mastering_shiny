0185ed63d2fcdb18e6f298e487b639ceacc2aed8
maplexuci <zhhxu9@gmail.com>
2021-03-30 23:38:08 -0400

    Finished the 1st half of Chapter 2

--- app.R
+++ app.R
@@ -1,8 +1,18 @@
 library(shiny)
 
 ui <- fluidPage(
-  dateInput("dob", "When were you born?"),
-  dateRangeInput("holiday", "When do you want to go on vacation next?")
+  textInput("name", "", placeholder = "Your name"),
+  sliderInput("date", "When should we deliver?", min = as.Date("2020-09-16"), max = as.Date("2020-09-23"), 
+              value = as.Date("2020-09-17"), timeFormat = "%F"),
+  sliderInput("number", "Automatic Number Selector", min = 0, max = 100, value = 0, step = 5,
+              animate = animationOptions(interval = 500, loop = TRUE, playButton = "Auto scroll")),
+
+  selectInput("Provience", "Choose a Province:",
+    list('North' = list("YK", "NT", "NU"),
+         'West' = list("BC", "AB", "MB"),
+         'South' = list("ON", "QB"),
+         'East' = list("NL", "NB", "NS", "PEI"))
+  )
 )
 
 server <- function(input, output, session) {
