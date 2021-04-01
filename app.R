39aeccbe9b508c2b5194994f827b1a09f370c304
maplexuci <zhhxu9@gmail.com>
2021-03-31 22:58:05 -0400

    Complete Chapter 2

--- app.R
+++ app.R
@@ -12,11 +12,35 @@ ui <- fluidPage(
          'West' = list("BC", "AB", "MB"),
          'South' = list("ON", "QB"),
          'East' = list("NL", "NB", "NS", "PEI"))
-  )
+  ),
+  
+  textOutput("text"),
+  verbatimTextOutput("code"),
+  
+  plotOutput("plot", width = "400px"),
+  
+  tableOutput("static"),
+  dataTableOutput("dynamic")
 )
 
 server <- function(input, output, session) {
+  # textOutput() corresponds to renderText()
+  output$text <- renderText({
+    "Hello friend!"
+  })
   
+  # verbatimTextOutput() corresponds to renderPrint()
+  output$code <- renderPrint({
+    summary(1:10)
+  })
+  
+  output$plot <- renderPlot(plot(1:5), res = 96)
+  
+  output$static <- renderTable(head(mtcars))
+  output$dynamic <- renderDataTable(mtcars, 
+    options = list(searching = FALSE, ordering = FALSE)
+    # options = list(pageLength = 5)
+    )
 }
 
 shinyApp(ui, server)
